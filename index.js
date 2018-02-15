19c0c2a7662db4cb0162173ea4e347d23fda843d
Nischay Ram Mamidi <NischayPro@gmail.com>
2018-02-16 03:32:04 +0530

    code: added downloading repo

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = (robot) => {
 	robot.on('pull_request.reopened', receive);
 	async function receive(context) {
@@ -12,12 +14,28 @@ module.exports = (robot) => {
 			// const config = await context.config('config.yml');
 			// console.log(config)
 			// if (config.reactComponentScreenShot) {
-			context.github.issues.createComment(context.issue({
-				body: 'Hi! This is a test commit'
-			}));
+			// context.github.issues.createComment(context.issue({
+			// 	body: 'Hi! This is a test commit'
+			// }));
 			// }
-			const files = await context.github.pullRequests.getFiles(context.issue());
-			console.log(files);
+			// const files = await context.github.pullRequests.getFiles(context.issue());
+			// console.log(context);
+			let repo = context.repo();
+			// let archive = Object.assign(repo, {
+			// 	sha: context.payload.pull_request.head,
+			// 	recursive: true
+			// });
+			let archive = Object.assign(context.repo(), {
+				archive_format: 'tarball',
+				ref: 'heads/master'
+			});
+			let writeStream = fs.createWriteStream('secret.zip'); 
+			const result = await context.github.repos.getArchiveLink(archive);
+			writeStream.write(result.data);
+			writeStream.on('finish', () => {  
+				console.log('wrote all data to file');
+			});
+			writeStream.end();  
 		} catch (err) {
 			if (err.code !== 404) {
 				throw err;
