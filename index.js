396aba81992b250ec981e462c152c07511fd83c4
Nischay Ram Mamidi <NischayPro@gmail.com>
2018-02-15 21:18:36 +0530

    code: getting git files

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
+	robot.on('pull_request.reopened', receive);
+	async function receive(context) {
+		// Get all issues for repo with user as creator
+		const response = await context.github.issues.getForRepo(context.repo({
+			state: 'all',
+			creator: context.payload.pull_request.user.login
+		}));
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+		const countPR = response.data.filter(data => data.pull_request);
+		try {
+			// const config = await context.config('config.yml');
+			// console.log(config)
+			// if (config.reactComponentScreenShot) {
+			context.github.issues.createComment(context.issue({
+				body: 'Hi! This is a test commit'
+			}));
+			// }
+			const files = await context.github.pullRequests.getFiles(context.issue());
+			console.log(files);
+		} catch (err) {
+			if (err.code !== 404) {
+				throw err;
+			}
+		}
+	}
 }
