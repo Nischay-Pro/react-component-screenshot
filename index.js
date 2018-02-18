62b285cd73ac48bac52eed7a2f8a41989ce7bf6c
Nischay Ram Mamidi <NischayPro@gmail.com>
2018-02-19 02:55:19 +0530

    code: added git merge feature

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const tar = require('tar-fs');
+const clone = require('git-clone');
+const path = require('path');
+const https = require('follow-redirects').https;
+const mkdirp = require('mkdirp');
 
 module.exports = (robot) => {
 	robot.on('pull_request.reopened', receive);
@@ -18,28 +23,66 @@ module.exports = (robot) => {
 			// 	body: 'Hi! This is a test commit'
 			// }));
 			// }
-			// const files = await context.github.pullRequests.getFiles(context.issue());
 			// console.log(context);
 			let repo = context.repo();
 			// let archive = Object.assign(repo, {
 			// 	sha: context.payload.pull_request.head,
 			// 	recursive: true
 			// });
-			let archive = Object.assign(context.repo(), {
-				archive_format: 'tarball',
-				ref: 'heads/master'
+			// let archive = Object.assign(context.repo(), {
+			// 	archive_format: 'tarball',
+			// 	ref: 'master'
+			// });
+
+			// let writeStream = fs.createWriteStream('secret.tar.gz'); 
+			// const result = await context.github.repos.getArchiveLink(archive);
+			// console.log(result);
+			// writeStream.write(result);
+			// writeStream.on('finish', () => {  
+			// 	console.log('wrote all data to file');
+			// });
+			// fs.createReadStream('secret.tar.gz').pipe(tar.extract('./test'));
+			// writeStream.end();  
+			clone('https://github.com/Nischay-Pro/playground.git', 'tmp/' + repo.owner + '/' + repo.repo, async (err, done) => {
+				if (!err) {
+					const files = await context.github.pullRequests.getFiles(context.issue());
+					let profn = []
+					files.data.map(element => {
+						profn.push(downloadFile(context, element));
+					});
+					Promise.all(profn).then(() => {
+						console.log('done');
+					}).catch(console.log(err));
+				} else console.log(err);
 			});
-			let writeStream = fs.createWriteStream('secret.zip'); 
-			const result = await context.github.repos.getArchiveLink(archive);
-			writeStream.write(result.data);
-			writeStream.on('finish', () => {  
-				console.log('wrote all data to file');
-			});
-			writeStream.end();  
+
 		} catch (err) {
 			if (err.code !== 404) {
 				throw err;
 			}
 		}
 	}
+}
+
+async function downloadFile(context, element) {
+	return new Promise((reject, resolve) => {
+		let file = {
+			name: path.basename(element.filename),
+			path: path.dirname(element.filename),
+			url: element.raw_url
+		}
+		mkdirp(file.path, function (err) {
+			if (err) {
+				console.error(err)
+				reject(err);
+			} else {
+				console.log(file.url);
+				let makefile = fs.createWriteStream(path.join('tmp', file.path, file.name));
+				let request = https.get(file.url, function (response) {
+					response.pipe(makefile);
+				});
+				resolve();
+			}
+		});
+	});
 }
